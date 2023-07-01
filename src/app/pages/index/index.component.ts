ac3f8261809a1ae8cf2c1ff315dc593d93e5251b
Hnaro <arhonprincipe16@gmail.com>
2023-07-01 16:50:33 -0600

    saved

---
+++ src/app/pages/index/index.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { WebBackendService } from '../../services/web-backend.service';
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
+})
+export class IndexComponent implements OnInit {
+  gamesSearchResults: any;
+  constructor(private clientAPI: WebBackendService) {
+    this.gamesSearchResults = [];
+  }
+  ngOnInit(): void {
+    let gameresults: any;
+    let results = this.clientAPI.getData().then(value => {
+      value.subscribe(value => {
+        this.gamesSearchResults = Object.values(value);
+        console.log(this.gamesSearchResults)
+      })
+    })
+  }
+}
