37e03ad11b5798c29c08b443ad7bfc9a0f8a972c
Hnaro Serrano <arhonprincipe16@gmail.com>
2023-07-01 22:23:53 -0600

    search bar component added no functions yet

---
+++ src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent {
+  onClick() {
+    // click send data that should trigger the search game
+    console.log("test");
+  }
+}
