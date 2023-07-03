1e10b1bc98a65be158f20e4c2c54e037acd5c8ed
Hnaro <arhonprincipe16@gmail.com>
2023-07-03 15:38:36 -0600

    updated

---
+++ src/.environment/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  deployedBackendURL: "http://52.60.90.150:7096/"
+}
--- src/environment/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  deployedBackendURL: "http://52.60.90.150:7096/"
-}
